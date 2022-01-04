ce5f481bd15cc7eedf1959973e29a07a6b6c0def
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-04 12:40:02 +0000

    added appollo client

--- client/index.js
+++ client/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import {ApolloClient} from 'react-apollo'
+
+const client = new ApolloClient({})
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloClient client={client}>
+      <div>Lyrical</div>
+    </ApolloClient>
+  )
 };
 
 ReactDOM.render(
