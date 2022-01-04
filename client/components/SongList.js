d069fdee232bb66c93410f0feeda4c4944b8b4af
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-04 13:18:44 +0000

    wrote first query

---
+++ client/components/SongList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import gql from 'graphql-tag';
+
+const SongList = () => {
+    return (
+        <div>
+            SongList
+        </div>
+    )
+}
+
+const query = gql`
+{
+    songs{
+        title
+    }
+}
+`
+
+export default SongList
